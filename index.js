3803e1abe7a4c63250dfc99aa5ea8fa707e1b5b5
unknown <alexalehno>
2021-02-09 21:17:40 +0300

    added index.html and additional task A4

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
-function a() {
-    console.log('a')
+"use strict";
+
+function reverse(p) {
+  console.log(p);
+  if (p === "") {
+    return "";
+  } else {
+    return reverse(p.slice(1)) + p[0];
+  }
 }
+
+function checkingForPalindrome() {
+  let string = prompt("", " h e l l o ьъеё.,/-_':;?!`~*").toLowerCase();
+  let ignoredСhar = " ьъеё.,/-_':;?!`~*";
+  let cleanedString = "";
+
+  for (let char of string) {
+    if (!ignoredСhar.includes(char)) {
+      cleanedString += char;
+    }
+  }
+
+  let reversedString = reverse(cleanedString);
+
+  console.log(cleanedString);
+  console.log(reversedString);
+  console.log(cleanedString === reversedString);
+}
+
+checkingForPalindrome();
