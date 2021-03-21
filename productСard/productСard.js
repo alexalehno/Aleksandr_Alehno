555f79c5c7b56c322e0e6cc51f38121ec05be1b9
unknown <alexalehno>
2021-03-21 22:05:06 +0300

    productCard

--- productСard/productСard.js
+++ productСard/productСard.js
@@ -1,26 +1,74 @@
 "use strict";
 
-function getData(event) {
+let arrData = [];
+let basket = [];
+
+function getData(arr) {
+  event.preventDefault();
+  let objData = {};
+  document.forms.form
+    .querySelectorAll("input")
+    .forEach((el) => (objData[el.name] = el.value));
+  arr.push(objData);
+  arr.forEach((el, i) => (el.id = i));
+  console.log(arr);
+}
+
+function render(arr) {
   event.preventDefault();
 
-  let objData = {};
-  document.querySelectorAll("input").forEach((el) => (objData[el.name] = el.value));
+  document.querySelectorAll(".item").forEach((el) => el.remove());
 
-  let str = `
-  <div>id: ${objData.id}</div>
-  <img src = "${objData.img}">
-  <div>цена: ${objData.price}</div>
-  <div>описание: ${objData.description}</div>`;
+  arr.forEach((el) => {
+    let div = document.createElement("div");
+    div.className = "item";
+    let strData = `
+      <div>id: ${el.id}</div>
+      <img src = "${el.img}">
+      <div><b>цена: ${el.price}</b></div>
+      <hr>
+      <div><b>описание:</b> ${el.description}</div>
+    `;
+    div.innerHTML = strData;
+    document.querySelector(".container").append(div);
 
-  let div = document.createElement("div");
-  div.className = "item";
-  div.innerHTML = str;
-  document.querySelector(".container").append(div);
+    let button = document.createElement("button");
+    button.innerHTML = "add";
+    div.append(button);
+
+    div.addEventListener("click", function (event) {
+      let basketDiv = document.querySelector(".basket");
+      
+      if (event.target === button && button.innerHTML === "add") {
+        button.innerHTML === "add"
+          ? (button.innerHTML = "delete")
+          : (button.innerHTML = "add");
+        basket.push(this);
+        basketDiv.append(this);
+      } else if (event.target === button && button.innerHTML === "delete") {
+        basket.pop(this);
+        this.remove();
+      }
+
+      console.log(basket);
+    });
+  });
 }
 
-function deleteItem() {
-  document.querySelector(".item").remove(); 
+function deleteItem(arr) {
+  event.preventDefault();
+
+  arr.pop();
+  render(arrData);
+  console.log(arr);
 }
 
-document.getElementById("button").addEventListener("click", getData);
-document.getElementById("buttonDelete").addEventListener("click", deleteItem);
+document.getElementById("saveBtn").addEventListener("click", () => {
+  getData(arrData);
+});
+document.getElementById("renderBtn").addEventListener("click", () => {
+  render(arrData);
+});
+document.getElementById("deleteBtn").addEventListener("click", () => {
+  deleteItem(arrData);
+});
