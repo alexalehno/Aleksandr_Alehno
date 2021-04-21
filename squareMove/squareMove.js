7bc96828aac04cf028889dc55c03f45747c75093
unknown <alexalehno>
2021-04-21 21:47:02 +0300

    squareMove

--- squareMove/squareMove.js
+++ squareMove/squareMove.js
@@ -5,22 +5,45 @@ const square = document.querySelector(".square");
 const btnMove = document.querySelector("#move");
 const btnStop = document.querySelector("#stop");
 
+let fieldStyle = getComputedStyle(field);
+let squareStyle = getComputedStyle(square);
+
+const stop = (timer) => clearInterval(timer);
+
+const update = () => {
+  square.style.left = initialCoordX + "px";
+  square.style.top = initialCoordY + "px";
+};
+
+let initialCoordX = parseInt(fieldStyle.width) / 2 - parseInt(squareStyle.width) / 2;
+let initialCoordY = parseInt(fieldStyle.height) / 2 - parseInt(squareStyle.height) / 2;
+let increase = 5;
+
+// window.addEventListener("keydown", move);
 btnMove.addEventListener("click", move);
 
+
 function move() {
+  let timer = setInterval(start , 1000 / 60);  
   btnStop.addEventListener("click", () => stop(timer));
-
-  let fieldStyle = getComputedStyle(field);
-  let squareStyle = getComputedStyle(square);
-  let start = parseInt(squareStyle.left);
-  const stop = (timer) => clearInterval(timer);
-
-  const timer = setInterval(() => {
-    if (start >= parseInt(fieldStyle.width) - parseInt(squareStyle.width)) {
-      start = 0;
-    }
-
-    square.style.left = `${start}px`;
-    start++;
-  }, 0);
 }
+
+
+function start() {
+  // if (e.code === 'ArrowLeft') {
+  //   initialCoordX += increase;
+  // }
+  // console.log(e.code)
+
+  initialCoordX += increase;
+  // initialCoordY += increase;
+
+  if (initialCoordX > parseInt(fieldStyle.width) - parseInt(squareStyle.width)) increase = -increase;
+  if (initialCoordX < 0) increase = -increase;
+  if (initialCoordY > parseInt(fieldStyle.height) - parseInt(squareStyle.height)) increase = -increase;
+  if (initialCoordY < 0) increase = -increase;
+
+  update();
+}
+
+update();
